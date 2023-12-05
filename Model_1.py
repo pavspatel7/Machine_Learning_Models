3f49278c38357b343894c64e5c6aba55be605c40
kushp15 <patelkush1508@gmail.com>
2023-12-05 18:11:53 -0500

    backup

--- Model_1.py
+++ Model_1.py
@@ -6,7 +6,6 @@ import matplotlib.pyplot as plt
 
 features = 2000
 # weight = np.random.uniform(-0.5, 0.5, features)
-bias = 0.5
 epoch = 500
 
 
@@ -14,31 +13,55 @@ def training_model(encoding, weight, target, epoch, alpha):
     # global weight, bias
     x_axis = []
     y_axis = []
+    bias = 0.5
     for e in range(epoch):
         each_epoch_loss = []
+        TrueCount = 0
+        avg = 0
         for x, s in zip(encoding, target):
             prediction = sigmoid_function(weight, x, bias)
+            TrueCount += 1  if classified_values(prediction) == s else 0
             loss = cross_entropy_loss(s, prediction)
             each_epoch_loss.append(loss)
-            weight = gradient_descent(x, s, prediction, alpha, bias, weight)
+            weight, bias = gradient_descent(x, s, prediction, alpha, bias, weight)
+            
+        avg = TrueCount / len(target)
         x_axis.append(e)
         y_axis.append(sum(each_epoch_loss) / len(each_epoch_loss))
-        print(f"Loss: {y_axis[e]} at time {e}")
-    plt.plot(x_axis, y_axis)
-    plt.xlabel('Time')
-    plt.ylabel('Loss')
-    plt.title('Loss at each time training 500 images')
-    # Displaying the graph
-    plt.show()
-    return weight
+        print(f"Loss: {y_axis[e]} at time {e}  Average: {avg}")
+    return weight, x_axis, y_axis, bias
 
 
-def logistic_model(encoding, target, weight):
+# def logistic_model(encoding, target, weight):
+#     for x, s in zip(encoding, target):
+#         prediction = sigmoid_function(weight, x, bias)
+#         loss = cross_entropy_loss(s, prediction)
+#         print(f"Prediction: {prediction: <20} Loss: {loss: 10}")
+
+
+def classified_values(prediction):
+    if prediction > 0.5:
+        return 1
+    else:
+        return 0
+
+def test_model(encoding, target, weight, bias):
+    new_y = []
+    new_x = []
+    i = 0
+    TrueCount = 0
+    print(weight)
     for x, s in zip(encoding, target):
         prediction = sigmoid_function(weight, x, bias)
+        TrueCount += 1  if classified_values(prediction) == s else 0
         loss = cross_entropy_loss(s, prediction)
-        print(f"Prediction: {prediction: <20} Loss: {loss: 10}")
-
+        new_y.append(loss)
+        new_x.append(i)
+        i+=1
+        print(f"Prediction: {prediction: <20} Target: {s: < 10} Loss: {loss: 10}")
+    avg = TrueCount / len(target)
+    print(f"avg {avg}")
+    return new_x, new_y
 
 def sigmoid_function(weight, x_vector, bias):
     weight_sum = np.dot(weight, x_vector) + bias
@@ -54,10 +77,13 @@ def cross_entropy_loss(targeted_value, predicted_value):
 
 def gradient_descent(features, target, prediction, alpha, bias, weight):
     # np.set_printoptions(threshold=np.inf, linewidth=np.inf)
-    new_weights = []
-    # new_bias = bias + alpha * (target - prediction)
-    for x, w in zip(features, weight):
-        new_w = w + alpha * (target - prediction) * float(x)
-        new_weights.append(new_w)
-    return new_weights  # , new_bias
+    # new_weights = []
+    new_bias = bias + alpha * (target - prediction)
+    # for x, w in zip(features, weight):
+    #     new_w = w + alpha * (target - prediction) * float(x)
+    #     new_weights.append(new_w)
+    
+    new_weights = weight + alpha * np.dot(features, (target-prediction))
+    
+    return new_weights , new_bias
 
--- training_model.py
+++ training_model.py
@@ -17,8 +17,18 @@ weight = np.random.uniform(-0.5, 0.5, features)
 # print("start before")
 # logistic_model(encoding=column_values_encoding, target=column_values_status, weight=weight)
 print("Training")
-weight = training_model(encoding=column_values_encoding, weight= weight, target=column_values_status, epoch=500, alpha=0.01)
+weight, x,y , bias = training_model(encoding=column_values_encoding, weight= weight, target=column_values_status, epoch=50, alpha=0.01)
 # print("After")
-# logistic_model(encoding=column_values_encoding, target=column_values_status, weight=weight)
+
+N_weight = weight
+N_bias = bias
+
+df = pd.read_excel('encoding_test.xlsx')
+df['Encoding'] = df['Encoding'].apply(ast.literal_eval)
+# Assuming the column you want to print is named 'Encoding'
+column_values_encoding = df['Encoding'].tolist()
+column_values_status = df['Status'].tolist()
+
+x,y = test_model(encoding=column_values_encoding, target=column_values_status, weight=N_weight, bias= N_bias)
 
 
